6280f63a794933e24c7f09fffa5dfe32db975ed6
Michael Kiggen <flawlesstreadgaming@gmail.com>
2025-09-28 19:10:14 +0200

    Adjusted container

--- src/components/containers/GlowContainer.tsx
+++ src/components/containers/GlowContainer.tsx
@@ -9,19 +9,18 @@ const GlowContainer = ({
   asset: string;
   isPhone: boolean;
 }) => {
-  let color = 'drop-shadow-[0_0_100px_var(--color-accentBlue-20)]';
-  let shadow = 'hover:drop-shadow-[0_0_100px_var(--color-accentBlue-40)]';
-
-  if (!isBlue) {
-    color = 'drop-shadow-[0_0_100px_var(--color-accentPink-20)]';
-    shadow = 'hover:drop-shadow-[0_0_100px_var(--color-accentPink-40)]';
-  }
+  const color = isBlue
+    ? 'drop-shadow-[0_0_100px_var(--color-accentBlue-20)]'
+    : 'drop-shadow-[0_0_100px_var(--color-accentPink-20)]';
+  const shadow = isBlue
+    ? 'hover:drop-shadow-[0_0_100px_var(--color-accentBlue-40)]'
+    : 'hover:drop-shadow-[0_0_100px_var(--color-accentPink-40)]';
 
   return (
     <div
       className={`${
         isPhone ? '' : 'flex'
-      } justify-items-center bg-transparent items-center lg:w-1/3 xl:w-2/6 md:w-1/2 rounded-2xl aspect-square ${
+      } justify-center bg-transparent items-center lg:w-2/5 xl:w-1/3 md:w-1/2 rounded-2xl aspect-square ${
         className ?? ''
       }`}
     >

ee2efa851e480950c931246f029c7d92ea34734b
Michael Kiggen <flawlesstreadgaming@gmail.com>
2025-09-28 18:54:06 +0200

    Added flex for mobile

--- src/components/containers/GlowContainer.tsx
+++ src/components/containers/GlowContainer.tsx
@@ -20,7 +20,7 @@ const GlowContainer = ({
   return (
     <div
       className={`${
-        isPhone ? '' : 'flex'
+        isPhone ? 'sm:flex' : 'flex'
       } justify-items-center bg-transparent items-center lg:w-1/3 xl:w-2/6 md:w-1/2 rounded-2xl aspect-square ${
         className ?? ''
       }`}
